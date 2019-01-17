2a48a3854531dff2bc67223ea29aaf4b9562eb1c
cocoa <cocoa.zkk@foxmail.com>
2019-01-17 14:40:45 +0800

    支持iOS8.0

--- KKPaddingLabel.podspec
+++ KKPaddingLabel.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "KKPaddingLabel"
-  s.version      = "1.0.1"
+  s.version      = "1.0.2"
   s.summary      = "简单的，可以给Label添加内边距，支持autolayout"
 
   # This description is used to generate tags and improve search results.
@@ -67,10 +67,10 @@ Pod::Spec.new do |s|
   # s.platform     = :ios, "5.0"
 
   #  When using multiple platforms
-    s.ios.deployment_target = "9.0"
-  # s.osx.deployment_target = "10.7"
-  # s.watchos.deployment_target = "2.0"
-  # s.tvos.deployment_target = "9.0"
+    s.ios.deployment_target = "8.0"
+    s.osx.deployment_target = "10.7"
+    s.watchos.deployment_target = "2.0"
+    s.tvos.deployment_target = "9.0"
 
 
   # ――― Source Location ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
--- PaddingLabel/ViewController.m
+++ PaddingLabel/ViewController.m
@@ -33,10 +33,11 @@
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:paddingLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:200.0f]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:paddingLabel attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.0 constant:100.0f]];
     
-//    [paddingLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.top.equalTo(self.view).with.offset(200);
-//        make.left.equalTo(self.view).with.offset(100);
-//    }];
+    [paddingLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view).with.offset(200);
+        make.left.equalTo(self.view).with.offset(100);
+    }];
+    
 }
 
 - (void)viewDidAppear:(BOOL)animated {
